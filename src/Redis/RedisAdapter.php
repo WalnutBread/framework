7b8183333bb7bc4d95e36081cd4e91302a8392f8
Geol2 <big9401@gmail.com>
2023-07-24 16:53:31 +0900

    MOD: RedisAdapter

--- src/Redis/RedisAdapter.php
+++ src/Redis/RedisAdapter.php
@@ -1,25 +1,17 @@
 <?php
 
-use Dotenv\Dotenv;
-
 class RedisAdapter
 {
-    public static string $ip = '';
-
-    public static string $port = '';
-
-    public static string $auth = '';
-
-    public static $redis;
+    public static $redisApp;
 
     public function __construct() { }
 
-    public static function setup(int $db) {
+    public static function setup(string $ip, string $port, string $auth = null, int $db) {
         try {
-            self::$redis = new Redis();
-            self::$redis->connect(self::$ip, self::$port, 2.5, NULL, 150);
-            self::$redis->auth(self::$auth);
-            self::$redis->select($db);
+            self::$redisApp = new Redis();
+            self::$redisApp->connect($ip, $port, 2.5, NULL, 150);
+            self::$redisApp->auth($auth);
+            self::$redisApp->select($db);
         } catch (\RedisException $e) {
             var_dump("레디스 접속 정보가 잘못되었습니다.");
             exit(0);
@@ -27,6 +19,11 @@ class RedisAdapter
     }
 
     public function zIncrBy(string $key, int $value, string $member) {
-        self::$redis->zIncrBy($key, $value, $member);
+        self::$redisApp->zIncrBy($key, $value, $member);
     }
+
+    public function get(string $key) {
+        return self::$redisApp->get($key);
+    }
+
 }
