f0bd557a00212f62535922214c241a1931d20704
Geol2 <big9401@gmail.com>
2023-07-25 14:45:23 +0900

    MOD : Application.php

--- src/Application.php
+++ src/Application.php
@@ -11,10 +11,10 @@ class Application
     public function __construct($providers = [])
     {
         $this->providers = $providers;
-        array_walk($this->providers, fn ($provider) => $provider->register());
+        array_walk($this->providers, fn ($provider) => $provider::register());
     }
 
     public function boot() {
-        array_walk($this->providers, fn ($provider) => $provider->boot());
+        array_walk($this->providers, fn ($provider) => $provider::boot());
     }
 }
