e03dae31be17e0e8913a4683ceed0656d3c9542b
Geol2 <big9401@gmail.com>
2023-08-03 14:10:45 +0900

    MOD CURL tossCurl

--- src/Curl/TossCurl.php
+++ src/Curl/TossCurl.php
@@ -4,24 +4,28 @@ namespace WalnutBread\Curl;
 
 class TossCurl
 {
-    public static function post($url, $credential, $postData): array {
+    public static function post($url, $credential, $postData): string | array
+    {
         $curlHandle = curl_init($url);
 
         curl_setopt_array($curlHandle, [
-            CURLOPT_POST => TRUE,
-            CURLOPT_RETURNTRANSFER => TRUE,
-            CURLOPT_HTTPHEADER => [
-                'Authorization: Basic ' . $credential,
-                'Content-Type: application/json'
-            ],
-            CURLOPT_POSTFIELDS => json_encode($postData)
+                CURLOPT_POST => TRUE,
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_HTTPHEADER => [
+                        'Authorization: Basic ' . $credential,
+                        'Content-Type: application/json'
+                ],
+                CURLOPT_POSTFIELDS => json_encode($postData),
+                CURLOPT_FAILONERROR => true,
         ]);
         $httpCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
         $response = curl_exec($curlHandle);
-
+        if( curl_error($curlHandle) ) {
+            return curl_error($curlHandle);
+        }
         return [
-            "code" => $httpCode,
-            "response" => $response
+                "code" => $httpCode,
+                "response" => $response
         ];
     }
 }
