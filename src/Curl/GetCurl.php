438bac512398bed1490f513680a1183d6cd5fee4
Geol2 <big9401@gmail.com>
2023-08-07 15:36:53 +0900

    Add Curl::get()

--- src/Curl/GetCurl.php
+++ src/Curl/GetCurl.php
@@ -4,5 +4,25 @@ namespace WalnutBread\Curl;
 
 class GetCurl
 {
+    public static function get($url, $getData, $header = null) {
+        $url = $url."?".http_build_query($getData, '', '&');
 
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_FAILONERROR, true);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        if( curl_error($curl) ) {
+            return curl_error($curl); # string
+        }
+
+        return [
+                "code" => $httpCode,
+                "response" => $response
+        ];
+    }
 }
