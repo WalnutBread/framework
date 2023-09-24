bf068f9d6388a5145feded017ee7fe58ff43345e
geol <big9401@gmail.com>
2023-09-24 12:30:18 +0900

    * 탭 변경

--- src/Curl/TossCurl.php
+++ src/Curl/TossCurl.php
@@ -9,14 +9,14 @@ class TossCurl
         $curlHandle = curl_init($url);
 
         curl_setopt_array($curlHandle, [
-                CURLOPT_POST => TRUE,
-                CURLOPT_RETURNTRANSFER => TRUE,
-                CURLOPT_HTTPHEADER => [
-                        'Authorization: Basic ' . $credential,
-                        'Content-Type: application/json'
-                ],
-                CURLOPT_POSTFIELDS => json_encode($postData),
-                CURLOPT_FAILONERROR => true,
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => [
+                    'Authorization: Basic ' . $credential,
+                    'Content-Type: application/json'
+            ],
+            CURLOPT_POSTFIELDS => json_encode($postData),
+            CURLOPT_FAILONERROR => true,
         ]);
         $httpCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
         $response = curl_exec($curlHandle);
